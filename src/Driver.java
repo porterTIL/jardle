7e986096f52aa219e5b7ce5c551cd84039efbc58
unknown <tristan.porter@tlgcohort.com>
2022-02-25 15:13:26 -0500

    :sparkles: user gets 6 tries to input a 5 letter word and word is checked against the secret word

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,5 @@
 public class Driver {
     public static void main(String[] args) {
         Wordle wordle = new Wordle();
-        for (String word : wordle.userGuesses){
-            System.out.println(word);
-        }
     }
 }
--- src/Wordle.java
+++ src/Wordle.java
@@ -1,10 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Wordle {
-    String secretWord; // initialize with word from library
+    String secretWord;
     String [] userGuesses = new String[5];
 
-    static {
+    Wordle(){
         System.out.println("To play wordle: enter a five letter word. Hit enter to continue");
+        try {
+            Scanner generateWord = new Scanner(new File("C:\\StudentWork\\IntmJ\\workspace\\jardle\\src\\en_dictionary.csv")).useDelimiter(",");
+            secretWord = generateWord.next();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        for (int tries = 0; tries < 6; tries++) {
+            Scanner userInput = new Scanner(System.in);
+            String userTry = userInput.nextLine();
+            if ( userTry.length() != 5 ){
+                System.out.println("Valid words are exactly 5 characters long");
+                System.out.println("Try has not counted");
+                tries--;
+                continue;
+            }
+            if (userTry.equals(secretWord)){
+                System.out.println("You win");
+                return;
+            }
+            System.out.println("You entered string: " + userTry);
+        }
     }
-
-
 }

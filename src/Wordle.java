9110cb0f3bc7e9c354ac3903b6b35e326a91b3dd
Adrian Velasco-Contreras <97752784+Chika2007@users.noreply.github.com>
2022-02-25 20:42:43 -0600

    Update Wordle Class and added 4 more words to dictionary
    Updated scanner to pull words into array list and pick random word from array list.

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,5 +1,9 @@
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Wordle {
@@ -7,14 +11,21 @@ public class Wordle {
     String [] userGuesses = new String[5];
 
     Wordle(){
+        List<String> words  = new ArrayList<>();
         System.out.println("To play wordle: enter a five letter word. Hit enter to continue");
         try {
-            Scanner generateWord = new Scanner(new File("jardle/src/en_dictionary.csv").getAbsoluteFile()).useDelimiter(",");
-            secretWord = generateWord.next();
+            Scanner wordList = new Scanner(new FileReader("src/en_dictionary.csv"));
+
+            while (wordList.hasNext()) {
+                secretWord = wordList.next();
+                words.add(secretWord);
+            }
+            String[] array = words.toArray(new String[0]);
+            secretWord = array[(int) (Math.random() * array.length)];
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
 
         for (int tries = 0; tries < 6; tries++) {
             Scanner userInput = new Scanner(System.in);
@@ -25,7 +36,7 @@ public class Wordle {
                 tries--;
                 continue;
             }
-            if (userTry.equalsIgnoreCase(secretWord)){
+            if (userTry.equalsIgnoreCase(this.secretWord)){
                 System.out.println("You win");
                 return;
             }

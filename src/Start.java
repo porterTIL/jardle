ffc5a948791001040bdbdebe9195725e8911b6d6
mxmurphy <97920912+mxmurphy@users.noreply.github.com>
2022-02-28 15:01:40 -0600

    changed instructions to individual println's in order to add a pause between lines

--- src/Start.java
+++ src/Start.java
@@ -20,17 +20,17 @@ public class Start {
     //still want to put a delay between lines of instructions
 
     public static void printInstructions() {
-        System.out.println("Welcome to Jardle! \n" +
-                "The task is to guess the five letter word within six tries. \n" +
-                "The guesssed letters will be either grey, yellow, or green. \n" +
-                "Green letters are in the correct position. \n" +
-                "Yellow letters are in the word, but in the wrong place. \n" +
-                "Grey letters are not located within the word.\n" +
-                "After solving each word, follow the directions to choose the next room and monster to defeat! \n" +
-                "Failing to solve the word in six tries means you lose (and start over) \n" +
-                "GOOD LUCK!"
-        );
+        System.out.println("Welcome to Jardle!");
+        System.out.println("The task is to guess the five letter word within six tries.");
+        System.out.println("The guesssed letters will be either grey, yellow, or green.");
+        System.out.println("Green letters are in the correct position.");
+        System.out.println("Yellow letters are in the word, but in the wrong place.");
+        System.out.println("Grey letters are not located within the word.");
+        System.out.println("After solving each word, follow the directions to choose the next room and monster to defeat!");
+        System.out.println("Failing to solve the word in six tries means you lose (and start over)!");
+        System.out.println("GOOD LUCK!");
     }
+
     //start new game...  somehow.
     public static void startGame() {
 

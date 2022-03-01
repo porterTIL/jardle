2cb32c50361a761f4e4ccf82e77075b83c0afcaf
Adrian Velasco-Contreras <97752784+Chika2007@users.noreply.github.com>
2022-03-01 08:58:12 -0600

    Update Avatar.java
    Addes players health method and death meathod

--- src/Avatar.java
+++ src/Avatar.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
+import java.awt.Color;
 
 public class Avatar {
-    int playerHP;
+    int playerHP = 3;
     String playerName;
     // String playerInventory;
     Scanner scan = new Scanner(System.in);
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED_LETTER = "\u001B[31m";
+    public static final String ANSI_CYAN_LETTER = "\u001B[36m";
 
     public void playerStart() {
-        playerHP = 3;
         System.out.println("Please enter your player names: ");
         playerName = scan.nextLine();
         System.out.println("Welcome " + playerName + " , to the world of\n");
-        System.out.println(".----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. \n" +
+        System.out.println(ANSI_CYAN_LETTER + ".----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. \n" +
                 "| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |\n" +
                 "| |     _____    | || |      __      | || |  _______     | || |  ________    | || |   _____      | || |  _________   | |\n" +
                 "| |    |_   _|   | || |     /  \\     | || | |_   __ \\    | || | |_   ___ `.  | || |  |_   _|     | || | |_   ___  |  | |\n" +
@@ -22,9 +25,16 @@ public class Avatar {
                 "| |  `.___.'     | || ||____|  |____|| || | |____| |___| | || | |________.'  | || |  |________|  | || | |_________|  | |\n" +
                 "| |              | || |              | || |              | || |              | || |              | || |              | |\n" +
                 "| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |\n" +
-                " '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' ");
+                " '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' "
+                + ANSI_RESET);
     }
+
+    public void playerHealth() {
+        playerHP = playerHP - 1;
+    }
+
     public void deadPlayer() {
-        
+        System.out.println("You have " + ANSI_RED_LETTER + "DIED!");
+        System.exit(0);
     }
 }

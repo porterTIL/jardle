fa24f648587a4be4bac4a170b39af21a120764ee
Adrian Velasco-Contreras <97752784+Chika2007@users.noreply.github.com>
2022-03-02 15:01:42 -0600

    Update Start.java
    Added timing delay to text and color

--- src/Start.java
+++ src/Start.java
@@ -1,4 +1,8 @@
 public class Start {
+    public static final String ANSI_YELLOW_LETTER = "\u001B[33m";
+    public static final String ANSI_GREEN_LETTER = "\u001B[32m";
+    public static final String ANSI_RED_LETTER = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
     public static void title() {
         System.out.println("       _               _ _      ");
@@ -11,15 +15,28 @@ public class Start {
 
     // TODO: add a delay before printing next instruction
     public static void printInstructions() {
-        System.out.println("Welcome to Jardle! \n");
-        System.out.println("The task is to guess the five letter word within six tries.");
-        System.out.println("The guessed letters will be either grey, yellow, or green.");
-        System.out.println("Green letters are in the correct position.");
-        System.out.println("Yellow letters are in the word, but in the wrong place.");
-        System.out.println("Grey letters are not located within the word.");
-        System.out.println("After solving each word, follow the directions to choose the next room and monster to defeat!");
-        System.out.println("Failing to solve the word in six tries means you lose (and start over)!");
-        System.out.println("GOOD LUCK!");
+        try {
+            Thread.sleep(1000);
+            System.out.println("Welcome to Jardle! \n");
+            Thread.sleep(1000);
+            System.out.println("The task is to guess the five letter word within six tries.");
+            Thread.sleep(1000);
+            System.out.println("The guessed letters will be either grey, " + ANSI_YELLOW_LETTER + "yellow" + ANSI_RESET
+                    + ", or " + ANSI_GREEN_LETTER + "green" + ANSI_RESET + ".");
+            Thread.sleep(1000);
+            System.out.println(ANSI_GREEN_LETTER + "Green" + ANSI_RESET + " letters are in the correct position.");
+            Thread.sleep(1000);
+            System.out.println(ANSI_YELLOW_LETTER + "Yellow" + ANSI_RESET + " letters are in the word, but in the wrong place.");
+            Thread.sleep(1000);
+            System.out.println("Grey letters are not located within the word.");
+            Thread.sleep(1000);
+            System.out.println("After solving each word, follow the directions to choose the next room and monster to defeat!");
+            Thread.sleep(1000);
+            System.out.println("Failing to solve the word in six tries means you lose " + ANSI_RED_LETTER +
+                    "(and START OVER)!" + ANSI_RESET);
+            Thread.sleep(1000);
+            System.out.println(ANSI_YELLOW_LETTER + "GOOD LUCK!" + ANSI_RESET);
+        } catch(InterruptedException e) {}
     }
 
     // TODO: hit any key to start new game

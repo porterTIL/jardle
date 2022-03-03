bbfd62742ba0a982613579f38ee1b3167ba62631
amy-upthagrove <amyupthagrove@gmail.com>
2022-03-03 14:16:41 -0800

    :sparkles: users can log in

--- src/com/verifyLogin/UserDriver.java
+++ src/com/verifyLogin/UserDriver.java
@@ -1,15 +1,8 @@
 package com.verifyLogin;
 
-import java.util.Scanner;
-
 public class UserDriver {
-    private static Scanner x;
 
     public static void main(String[] args) {
-        String username = "Amy123";
-        String password = "password";
-        String filepath = "C:\\Java StudentWork pt1\\IntroJava\\workspace\\jardle\\src\\com\\verifyLogin\\users.txt";
-
-        User.verifyLogin(username, password);
+        User.verifyLogin();
     }
 }
